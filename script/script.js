a25876ec649ce9b121eb72008289274ddf5e6f76
Alexandr Dmitrenko <a.akd4783@gmail.com>
2022-10-17 11:47:29 +0300

    add js

--- script/script.js
+++ script/script.js
@@ -1,82 +1,120 @@
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
+//поле игры
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+//результаты
+const result = document.getElementById('result');
+const total = document.getElementById('total')
+//кнопка перезагрузки игры
+const restart = document.getElementById('restart');
+// Создаем картинку
+const img = new Image();
+img.src = '/img/flappy_bird_bird.png'
 
-const bird = new Image();
-const bg = new Image();
-const fg = new Image();
-const pipeUp = new Image();
-const pipeBottom = new Image();
+let birdX = 200;
+let birdY = 100;
+let birdTime = null;
+let stop = 0
 
-bird.src = "/img/flappy_bird_bird.png";
-bg.src = "/img/flappy_bird_bg.png";
-fg.src = "/img/flappy_bird_fg.png";
-pipeUp.src = "/img/flappy_bird_pipeUp.png";
-pipeBottom.src = "/img/flappy_bird_pipeBottom.png";
+//запускаем игру
+function loadGame() {
+    img.onload = function () {
+        if (birdTime === null && stop === 0) {
+            birdTime = setInterval(function () {
 
-let gap = 90;
+                if (birdY <= 350) {
+                    birdY++
+                }
 
-// При нажатии на какую-либо кнопку
-document.addEventListener("mousedown", moveUp);
-
-function moveUp() {
-    yPos -= 40;
-    console.log(yPos)
-}
-
-// Создание блоков
-let pipe = [];
-
-pipe[0] = {
-    x : canvas.width,
-    y : 0
-}
-
-const score = 0;
-// Позиция птички
-var xPos = 10;
-var yPos = 150;
-var grav = 1.5;
-
-function draw() {
-    context.drawImage(bg, 0, 0);
-
-    for(let i = 0; i < pipe.length; i++) {
-        context.drawImage(pipeUp, pipe[i].x, pipe[i].y);
-        context.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
-
-        pipe[i].x--;
-
-        if(pipe[i].x == 125) {
-            pipe.push({
-                x : canvas.width,
-                y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
-            });
-        }
-
-        // Отслеживание прикосновений
-        if(xPos + bird.width >= pipe[i].x
-            && xPos <= pipe[i].x + pipeUp.width
-            && (yPos <= pipe[i].y + pipeUp.height
-                || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= canvas.height - fg.height) {
-            location.reload(); // Перезагрузка страницы
-        }
-
-        if(pipe[i].x == 5) {
-            score++;
-            // score_audio.play();
+                context.clearRect(0, 0, 800, 400)
+                drawColumn()
+                context.drawImage(img, 0, 0, 52, 45, birdX, birdY, 52, 45);
+            }, 10)
         }
     }
-
-    // context.drawImage(fg, 0, canvas.height - fg.height);
-    context.drawImage(bird, xPos, yPos);
-
-    yPos += grav;
-
-    // context.fillStyle = "#000";
-    // context.font = "24px Verdana";
-    // context.fillText("Счет: " + score, 10, canvas.height - 20);
-
-    requestAnimationFrame(draw);
 }
 
-pipeBottom.onload = draw;
+let click = null
+
+document.onmouseup = function () {
+    if (stop === 0) {
+        context.drawImage(img, 0, 0, 52, 45, birdX, birdY, 52, 45);
+        clearInterval(click)
+    }
+}
+
+document.onmousedown = function () {
+    if (stop === 0) {
+        click = setInterval(function () {
+            context.drawImage(img, 104, 0, 52, 45, birdX, birdY, 52, 45);
+            birdY = birdY - 2
+        }, 10)
+    }
+}
+// столб
+let columnArr = []
+let columnTimer = null
+
+function createColumn() {
+    columnTimer = setInterval(function () {
+        let column = {};
+        column.postionX = 800;
+        column.postionY = -Math.round(Math.random() * 100 + 150);
+        column.imgA = new Image();
+        column.imgB = new Image();
+        column.imgA.src = '/img/flappy_bird_pipeBottom.png';
+        column.imgB.src = '/img/flappy_bird_pipeUp.png';
+        column.id = new Date().getTime();
+
+        columnArr.push(column);
+
+    }, 1500)
+}
+
+//создаем колонны
+createColumn();
+
+let same = null;
+let score = 0;
+
+function drawColumn() {
+    for (let i = 0; i < columnArr.length; i++) {
+        columnArr[i].postionX--
+        context.drawImage(columnArr[i].imgA, columnArr[i].postionX, columnArr[i].postionY - 100);
+        context.drawImage(columnArr[i].imgB, columnArr[i].postionX, columnArr[i].postionY + 450);
+
+        if (birdX + 40 >= columnArr[i].postionX && birdX - 70 <= columnArr[i].postionX) {
+            if (columnArr[i].id != same) {
+                score++;
+                same = columnArr[i].id;
+                let record = localStorage.getItem('record');
+                if (score > record) {
+                    localStorage.setItem('record', score);
+                }
+                result.innerText = score;
+            }
+
+            // Определяем столкновение
+            if (birdY < columnArr[i].postionY + 300 || birdY + 35 > columnArr[i].postionY + 450) {
+                clearInterval(columnTimer)
+                clearInterval(birdTime)
+                stop = 1
+                result.innerText = score;
+                let restartBox = document.querySelector('.box__restart');
+                restartBox.classList.remove('box__restart--active');
+                let record = localStorage.getItem('record');
+                if (score > record) {
+                    localStorage.setItem('record', score);
+                }
+            }
+        }
+    }
+}
+
+restart.addEventListener('click', () => {
+    location.reload();
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadGame();
+    total.innerText = localStorage.getItem('record');
+})
